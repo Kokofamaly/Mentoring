e43948f32058a716874f64dec4825c891f18b9c8
Kokofamaly <fateev1vv2@gmail.com>
2026-03-04 15:19:40 +0100

    first commit

---
+++ Lesson 1/Task1Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task1Console;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string username = args.Length > 0 ? args[0] : "Undefined";
+        Console.WriteLine($"Hello, {username}");
+    }
+}
