4add59195a4359fc412836646f346210f7c35534
Kokofamaly <fateev1vv2@gmail.com>
2026-03-04 19:51:35 +0100

    task 3

---
+++ Lesson 1/Task3/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Task3;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Lesson 1/Task3/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task3;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnClicked(object sender, EventArgs e)
+    {
+        string username = UserInput.Text ?? "User";
+        await DisplayAlert("Greeting", $"Hello, {username}!", "OK");
+    }
+}
