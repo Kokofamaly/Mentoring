ed7cd7110ca2e914600e9559d26b8298c7ea37db
Вадим Фатеев <fateevdanon@gmail.com>
2026-03-04 17:00:08 +0100

    added WinForms

--- Lesson 1/Task1WinForms/Form1.cs
+++ Lesson 1/Task1WinForms/Form1.cs
@@ -1,3 +1,7 @@
+using Task2;
+using System;
+using System.Windows.Forms;
+
 namespace Task1WinForms;
 
 public partial class Form1 : Form
@@ -5,5 +9,11 @@ public partial class Form1 : Form
     public Form1()
     {
         InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        var hello = new HelloLibrary();
+        MessageBox.Show(hello.GetMessage(textBox1.Text));
     }
 }
