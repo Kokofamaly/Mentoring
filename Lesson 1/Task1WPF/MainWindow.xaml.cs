8dae65d2601728a29f279179ee1e6b20b9fb9e31
Вадим Фатеев <fateevdanon@gmail.com>
2026-03-04 16:49:27 +0100

    completed wpf task

--- Lesson 1/Task1WPF/MainWindow.xaml.cs
+++ Lesson 1/Task1WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Task2;
 
 
 
@@ -21,5 +23,11 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+    // hello
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        var hello = new HelloLibrary();
+        MessageBox.Show(hello.GetMessage(UserInput.Text));
     }
 }
