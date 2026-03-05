fe657535f5302cff12f2c98a24d9823b971c2e4d
Kokofamaly <fateev1vv2@gmail.com>
2026-03-05 10:11:40 +0100

    changed formatting of HelloLibrary.GetMessage output

--- Lesson 1/Task2/HelloLibrary.cs
+++ Lesson 1/Task2/HelloLibrary.cs
@@ -7,6 +7,6 @@ public class HelloLibrary
     public string GetMessage(string username)
     {
         if(String.IsNullOrEmpty(username)) username = "User";
-        return $"{DateTime.UtcNow}:Hello, {username}";
+        return $"{DateTime.UtcNow}:\n\tHello, {username}";
     }
 }
