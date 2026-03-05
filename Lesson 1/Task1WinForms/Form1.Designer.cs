1da143947a641f2d1b9869127a7fb818e255be56
Kokofamaly <fateev1vv2@gmail.com>
2026-03-05 10:03:15 +0100

    fixed winforms

--- Lesson 1/Task1WinForms/Form1.Designer.cs
+++ Lesson 1/Task1WinForms/Form1.Designer.cs
@@ -33,6 +33,11 @@ partial class Form1
         textBox1 = new();
         button1 = new();
 
+        textBox1.Location = new System.Drawing.Point(50, 50);
+        textBox1.Size = new System.Drawing.Size(200, 30);  
+
+        button1.Location = new System.Drawing.Point(50, 100);
+        button1.Size = new System.Drawing.Size(100, 40);
         button1.Text = "Greetings!";
         button1.Click += button1_Click;
         
