a16dec736df6a5ec06e84b259ece32fe3fde8f13
Kokofamaly <fateev1vv2@gmail.com>
2026-04-18 21:06:02 +0300

    Finish task 2 for lesson 8

---
+++ Lesson 8/SQLHomework/EmployeeInfo.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW [dbo].[EmployeeInfo]
+	AS SELECT Employee.Id, 
+	COALESCE (Employee.EmployeeName, CONCAT(Person.FirstName, ' ', Person.LastName)) AS EmployeeFullName, 
+	CONCAT(Address.ZipCode, '_', Address.State, ' ', Address.City, '-', Address.Street) AS EmployeeFullAddress,
+	CONCAT(Employee.CompanyName, ' ', Employee.Position) AS EmployeeCompanyInfo,
+	Employee.CompanyName, 
+	Address.City
+	FROM [dbo].[Employee]
+	JOIN [dbo].[Address] ON Address.Id = Employee.AddressId
+	JOIN [dbo].[Person] ON Person.Id = Employee.PersonId;
