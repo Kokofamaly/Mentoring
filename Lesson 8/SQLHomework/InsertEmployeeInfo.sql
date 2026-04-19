973985c9e52c0b485029d70213557ddff2672b73
Kokofamaly <fateev1vv2@gmail.com>
2026-04-19 15:52:05 +0300

    Start work on stored procedure

---
+++ Lesson 8/SQLHomework/InsertEmployeeInfo.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[InsertEmployeeInfo]
+	@EmployeeName NVARCHAR(100) = NULL,
+	@FirstName NVARCHAR(50) = NULL,
+	@LastName NVARCHAR(50) = NULL,
+	@CompanyName NVARCHAR(20),
+	@Position NVARCHAR(30) = NULL,
+	@Street NVARCHAR(50),
+	@City NVARCHAR(20) = NULL,
+	@State NVARCHAR(50) = NULL,
+	@ZipCode NVARCHAR(50) = NULL
+AS
+BEGIN
+END
