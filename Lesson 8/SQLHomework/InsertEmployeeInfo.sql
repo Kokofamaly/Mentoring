aca9cc298b01ca556b3ca1bba11636c77c9aa3d1
Kokofamaly <fateev1vv2@gmail.com>
2026-04-20 20:32:54 +0300

    Complete Task 3 of Lesson 8

--- Lesson 8/SQLHomework/InsertEmployeeInfo.sql
+++ Lesson 8/SQLHomework/InsertEmployeeInfo.sql
@@ -10,4 +10,38 @@
 	@ZipCode NVARCHAR(50) = NULL
 AS
 BEGIN
+	SET NOCOUNT ON;
+
+	    IF (TRIM(ISNULL(@EmployeeName, '')) = '' AND 
+        TRIM(ISNULL(@FirstName, '')) = '' AND 
+        TRIM(ISNULL(@LastName, '')) = '')
+    BEGIN
+        RAISERROR('At least one name field (EmployeeName, FirstName, or LastName) must be provided.', 16, 1);
+        RETURN;
+    END
+
+	DECLARE @TruncatedCompany NVARCHAR(20) = LEFT(@CompanyName, 20);
+
+	BEGIN TRY
+		BEGIN TRANSACTION;
+
+			INSERT INTO [dbo].[Address] (Street, City, State, ZipCode) 
+			VALUES (@Street, @City, @State, @ZipCode);
+
+			DECLARE @AddrId INT = SCOPE_IDENTITY();
+
+			INSERT INTO [dbo].[Person] (FirstName, LastName) 
+			VALUES(ISNULL(@FirstName, ''), ISNULL(@LastName, ''));
+
+			DECLARE @PersId INT = SCOPE_IDENTITY();
+		
+			INSERT INTO [dbo].[Employee] (AddressId, PersonId, CompanyName, Position, EmployeeName)
+			VALUES (@AddrId, @PersId, @TruncatedCompany, @Position, @EmployeeName);
+
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+		IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION;
+		THROW;
+	END CATCH
 END
