a041dbc80ad5ab8df7e9231b2701196b7ec5919b
Kokofamaly <fateev1vv2@gmail.com>
2026-04-29 21:58:48 +0200

    Complete lesson11-task2

--- Lesson 11/FileCabinetSoftware/Program.cs
+++ Lesson 11/FileCabinetSoftware/Program.cs
@@ -11,6 +11,15 @@ public class Program
     public static void Main(string[] args)
     {
 
-        
+        var service = new DocumentService(new FileRepository());
+        Document mag = service.GetDocument(Enums.DocumentType.Magazine, 1);
+
+        if(mag == null) throw new Exception();
+
+        var props = mag.GetType().GetProperties();
+        foreach(var prop in props)
+        {
+            Console.WriteLine(prop.Name + ": " + prop.GetValue(mag));
+        }
     }
 }
