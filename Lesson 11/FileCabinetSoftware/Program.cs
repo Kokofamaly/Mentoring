bc46a9c32b8b12f6d0aa2af112dfe181b494df6a
Kokofamaly <fateev1vv2@gmail.com>
2026-04-29 21:43:44 +0200

    Complete lesson11-task2

--- Lesson 11/FileCabinetSoftware/Program.cs
+++ Lesson 11/FileCabinetSoftware/Program.cs
@@ -10,24 +10,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Document locBook = new LocalizedBook
-        {
-            Title = "Clean Code",
-            DatePublished = new DateOnly(2008, 8, 1),
-            ISBN = "978-0132350884",
-            Authors = new List<string>
-            {
-                "Robert C. Martin"
-            },
-            OriginalPublisher = "Prentice Hall",
-            LocalPublisher = "ЭКСМО",
-            CountryOfLocalization = "Russia",
-            
-        };
-        var service = new DocumentService(new FileRepository());
-        service.Save(locBook);
-        var doc = service.GetDocument(Enums.DocumentType.LocalizedBook, 2);
-        Console.WriteLine("Title: " + doc?.Title);
+
         
     }
 }
