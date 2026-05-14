1ff209cd86ccf1191d41d99aafc49a4e8d9210f3
Kokofamaly <fateev1vv2@gmail.com>
2026-05-14 17:42:05 +0200

    Complete task 4 for lesson 8

--- Lesson 8/SQLHomework/InsertingEmployeeTrigger.sql
+++ Lesson 8/SQLHomework/InsertingEmployeeTrigger.sql
@@ -37,31 +37,3 @@ BEGIN
     FROM inserted i
     JOIN @NewAddresses na ON na.OldAddressId = i.AddressId;
 END
-
---     DECLARE @AddressId INT;
---     DECLARE @CompanyName NVARCHAR(20);
-
---     DECLARE employee_cursor CURSOR FOR 
---     SELECT AddressId, CompanyName FROM inserted;
-
---     OPEN employee_cursor;
---     FETCH NEXT FROM employee_cursor INTO @AddressId, @CompanyName;
-
---     WHILE @@FETCH_STATUS = 0
---     BEGIN
---         INSERT INTO [dbo].[Address] (Street, City, State, ZipCode)
---         SELECT Street, City, State, ZipCode 
---         FROM [dbo].[Address] 
---         WHERE Id = @AddressId;
-
---         DECLARE @NewAddrId INT = SCOPE_IDENTITY();
-
---         INSERT INTO [dbo].[Company] (Name, AddressId)
---         VALUES (@CompanyName, @NewAddrId);
-
---         FETCH NEXT FROM employee_cursor INTO @AddressId, @CompanyName;
---     END
-
---     CLOSE employee_cursor;
---     DEALLOCATE employee_cursor;
--- END
