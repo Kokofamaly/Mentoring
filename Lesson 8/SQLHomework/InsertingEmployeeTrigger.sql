e334d10b7d3877ab902fa4820a8b40dce4575c48
Kokofamaly <fateev1vv2@gmail.com>
2026-05-14 13:54:25 +0200

    Complete task 4 for lesson 8

--- Lesson 8/SQLHomework/InsertingEmployeeTrigger.sql
+++ Lesson 8/SQLHomework/InsertingEmployeeTrigger.sql
@@ -1,34 +1,67 @@
-﻿CREATE TRIGGER [dbo].[trg_Employee_Insert_CreateCompany]
+﻿CREATE OR ALTER TRIGGER [dbo].[trg_Employee_Insert_CreateCompany]
 ON [dbo].[Employee]
 AFTER INSERT
 AS
 BEGIN
     SET NOCOUNT ON;
     
-    DECLARE @AddressId INT;
-    DECLARE @CompanyName NVARCHAR(20);
+    DECLARE @ToInsert TABLE(
+        OldAddressId INT,
+        Street NVARCHAR(50),
+        City NVARCHAR(20),
+        State NVARCHAR(50),
+        ZipCode NVARCHAR(50)
+    );
 
-    DECLARE employee_cursor CURSOR FOR 
-    SELECT AddressId, CompanyName FROM inserted;
+    INSERT INTO @ToInsert (OldAddressId, Street, City, State, ZipCode)
+    SELECT a.Id, a.Street, a.City, a.State, a.ZipCode
+    FROM inserted i
+    JOIN [dbo].[Address] a ON a.Id = i.AddressId;
 
-    OPEN employee_cursor;
-    FETCH NEXT FROM employee_cursor INTO @AddressId, @CompanyName;
+    DECLARE @NewAddresses TABLE(
+        OldAddressId INT,
+        NewAddressId INT
+    );
 
-    WHILE @@FETCH_STATUS = 0
-    BEGIN
-        INSERT INTO [dbo].[Address] (Street, City, State, ZipCode)
-        SELECT Street, City, State, ZipCode 
-        FROM [dbo].[Address] 
-        WHERE Id = @AddressId;
+    MERGE INTO [dbo].[Address] AS target
+    USING @ToInsert AS src
+    ON 1 = 0
+    WHEN NOT MATCHED THEN
+        INSERT (Street, City, State, ZipCode)
+        VALUES (src.Street, src.City, src.State, src.ZipCode)
+    OUTPUT src.OldAddressId, INSERTED.Id
+    INTO @NewAddresses (OldAddressId, NewAddressId);
 
-        DECLARE @NewAddrId INT = SCOPE_IDENTITY();
-
-        INSERT INTO [dbo].[Company] (Name, AddressId)
-        VALUES (@CompanyName, @NewAddrId);
-
-        FETCH NEXT FROM employee_cursor INTO @AddressId, @CompanyName;
-    END
-
-    CLOSE employee_cursor;
-    DEALLOCATE employee_cursor;
+    INSERT INTO [dbo].[Company] (Name, AddressId)
+    SELECT i.CompanyName, na.NewAddressId
+    FROM inserted i
+    JOIN @NewAddresses na ON na.OldAddressId = i.AddressId;
 END
+
+--     DECLARE @AddressId INT;
+--     DECLARE @CompanyName NVARCHAR(20);
+
+--     DECLARE employee_cursor CURSOR FOR 
+--     SELECT AddressId, CompanyName FROM inserted;
+
+--     OPEN employee_cursor;
+--     FETCH NEXT FROM employee_cursor INTO @AddressId, @CompanyName;
+
+--     WHILE @@FETCH_STATUS = 0
+--     BEGIN
+--         INSERT INTO [dbo].[Address] (Street, City, State, ZipCode)
+--         SELECT Street, City, State, ZipCode 
+--         FROM [dbo].[Address] 
+--         WHERE Id = @AddressId;
+
+--         DECLARE @NewAddrId INT = SCOPE_IDENTITY();
+
+--         INSERT INTO [dbo].[Company] (Name, AddressId)
+--         VALUES (@CompanyName, @NewAddrId);
+
+--         FETCH NEXT FROM employee_cursor INTO @AddressId, @CompanyName;
+--     END
+
+--     CLOSE employee_cursor;
+--     DEALLOCATE employee_cursor;
+-- END
