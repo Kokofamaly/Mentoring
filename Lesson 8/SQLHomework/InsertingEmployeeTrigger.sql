8d79446526425b21be1da7bb15cc32e585499839
Kokofamaly <fateev1vv2@gmail.com>
2026-05-17 12:58:08 +0200

    New attempt to make trigger that works

--- Lesson 8/SQLHomework/InsertingEmployeeTrigger.sql
+++ Lesson 8/SQLHomework/InsertingEmployeeTrigger.sql
@@ -5,35 +5,7 @@ AS
 BEGIN
     SET NOCOUNT ON;
     
-    DECLARE @ToInsert TABLE(
-        OldAddressId INT,
-        Street NVARCHAR(50),
-        City NVARCHAR(20),
-        State NVARCHAR(50),
-        ZipCode NVARCHAR(50)
-    );
-
-    INSERT INTO @ToInsert (OldAddressId, Street, City, State, ZipCode)
-    SELECT a.Id, a.Street, a.City, a.State, a.ZipCode
-    FROM inserted i
-    JOIN [dbo].[Address] a ON a.Id = i.AddressId;
-
-    DECLARE @NewAddresses TABLE(
-        OldAddressId INT,
-        NewAddressId INT
-    );
-
-    MERGE INTO [dbo].[Address] AS target
-    USING @ToInsert AS src
-    ON 1 = 0
-    WHEN NOT MATCHED THEN
-        INSERT (Street, City, State, ZipCode)
-        VALUES (src.Street, src.City, src.State, src.ZipCode)
-    OUTPUT src.OldAddressId, INSERTED.Id
-    INTO @NewAddresses (OldAddressId, NewAddressId);
-
     INSERT INTO [dbo].[Company] (Name, AddressId)
-    SELECT i.CompanyName, na.NewAddressId
-    FROM inserted i
-    JOIN @NewAddresses na ON na.OldAddressId = i.AddressId;
+    SELECT i.CompanyName, i.AddressId
+    FROM inserted i;
 END
