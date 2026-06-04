bef9b42a260b0ae2c9ea2eae26b6a53a36f8d634
Kokofamaly <fateev1vv2@gmail.com>
2026-06-04 16:20:44 +0300

    Correct MapProduct method to avoid potential InvalidOperationException

--- Lesson 13/ShopSystem/Repositories/ProductsRepository.cs
+++ Lesson 13/ShopSystem/Repositories/ProductsRepository.cs
@@ -1,8 +1,9 @@
-﻿using ShopSystem.Infrastructure.Interfaces;
+﻿using Microsoft.Data.SqlClient;
+using ShopSystem.Infrastructure.Interfaces;
 using ShopSystem.Models;
 using System;
 using System.Collections.Generic;
-using Microsoft.Data.SqlClient;
+using System.Data;
 using System.Text;
 
 namespace ShopSystem.Repositories
@@ -91,7 +92,7 @@ namespace ShopSystem.Repositories
             {
                 Id = reader.GetInt32(reader.GetOrdinal("Id")),
                 Name = reader.GetString(reader.GetOrdinal("Name")),
-                Description = reader.GetString(reader.GetOrdinal("Description")),
+                Description = reader.IsDBNull(reader.GetString(reader.GetOrdinal("Description"))) ? null : reader.GetString(reader.GetOrdinal("Description")),
                 Weight = reader.GetDecimal(reader.GetOrdinal("Weight")),
                 Height = reader.GetDecimal(reader.GetOrdinal("Height")),
                 Width = reader.GetDecimal(reader.GetOrdinal("Width")),
