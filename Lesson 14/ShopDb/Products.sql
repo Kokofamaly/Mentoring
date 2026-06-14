c14aa01a4acad9d4779224bcade5830477aa6a0b
Kokofamaly <fateev1vv2@gmail.com>
2026-06-14 10:33:10 +0300

    Add db

--- Lesson 14/ShopDb/Products.sql
+++ Lesson 14/ShopDb/Products.sql
@@ -1,4 +1,10 @@
 ﻿CREATE TABLE [dbo].[Products]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [Description] NVARCHAR(MAX) NULL, 
+    [Weight] DECIMAL(10, 2) NOT NULL, 
+    [Height] DECIMAL(4, 2) NOT NULL, 
+    [Width] DECIMAL(4, 2) NOT NULL, 
+    [Length] DECIMAL(4, 2) NOT NULL
 )
