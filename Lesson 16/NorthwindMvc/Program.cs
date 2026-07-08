60a4e18558f55ab70339cc66191da2d6abf1716e
Kokofamaly <fateev1vv2@gmail.com>
2026-07-08 15:32:16 +0300

    Made db configuration in Program.cs insteand of NorthwindContext.cs

--- Lesson 16/NorthwindMvc/Program.cs
+++ Lesson 16/NorthwindMvc/Program.cs
@@ -1,7 +1,12 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using NorthwindMvc.Models;
 
+var builder = WebApplication.CreateBuilder(args);
+string connectionString = "Data Source=(localdb)\\\\MSSQLLocalDB;Initial Catalog=SQLHomework;Integrated Security=True;Connect Timeout=60;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30";
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<NorthwindContext>(options => options.UseSqlServer(connectionString));
+);
 
 var app = builder.Build();
 
