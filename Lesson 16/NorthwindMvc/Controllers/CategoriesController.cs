6486191718f153ae645cf20c6ad2d2313f92ccdf
Kokofamaly <fateev1vv2@gmail.com>
2026-07-08 19:39:42 +0300

    Complete task 1
    Add implementation of views for Products- and CategoriesController,

--- Lesson 16/NorthwindMvc/Controllers/CategoriesController.cs
+++ Lesson 16/NorthwindMvc/Controllers/CategoriesController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NorthwindMvc.Models;
 
 namespace NorthwindMvc.Controllers
 {
     public class CategoriesController : Controller
     {
+        private readonly NorthwindContext _context;
+        public CategoriesController(NorthwindContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var categories = _context.Categories.ToList();
+            return View(categories);
         }
     }
 }
--- Lesson 16/NorthwindMvc/Controllers/HomeController.cs
+++ Lesson 16/NorthwindMvc/Controllers/HomeController.cs
@@ -11,11 +11,6 @@ namespace NorthwindMvc.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
