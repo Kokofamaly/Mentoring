2650da4fb0137e8551f9bf9610664cb0ff415e78
Kokofamaly <fateev1vv2@gmail.com>
2026-07-09 15:05:19 +0300

    Complete task 3
    Add GET and POST actions 'Create' and 'Update', also add views for them. Add buttons on Products/Index view to create and update products

--- Lesson 16/NorthwindMvc/Controllers/ProductsController.cs
+++ Lesson 16/NorthwindMvc/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using NorthwindMvc.Models;
 
@@ -24,6 +25,63 @@ namespace NorthwindMvc.Controllers
             }
             var productsAll = productsQuery.ToList();
             return View(productsAll);
+        }
+        public IActionResult Update(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null) return NotFound();
+
+            PopulateDropDowns(product);
+            return View(product);
+        }
+        public IActionResult Create()
+        {
+            PopulateDropDowns();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Update(int id, Product product) {
+            if(id != product.ProductId) return BadRequest();
+
+            if (!ModelState.IsValid)
+            {
+                PopulateDropDowns(product);
+                return View(product);
+            }
+            var existingProduct = _context.Products.Find(id);
+            if (existingProduct == null) return NotFound();
+
+            existingProduct.ProductName = product.ProductName;
+            existingProduct.SupplierId = product.SupplierId;
+            existingProduct.CategoryId = product.CategoryId;
+            existingProduct.QuantityPerUnit = product.QuantityPerUnit;
+            existingProduct.UnitPrice = product.UnitPrice;
+            existingProduct.UnitsInStock = product.UnitsInStock;
+            existingProduct.UnitsOnOrder = product.UnitsOnOrder;
+            existingProduct.ReorderLevel = product.ReorderLevel;
+            existingProduct.Discontinued = product.Discontinued;
+
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product) {
+            if (!ModelState.IsValid)
+            {
+                PopulateDropDowns(product);
+                return View(product);
+            }
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private void PopulateDropDowns(Product? product = null)
+        {
+            ViewBag.Categories = new SelectList(_context.Categories.OrderBy(c => c.CategoryName), "CategoryId", "CategoryName", product?.CategoryId);
+            ViewBag.Suppliers = new SelectList(_context.Suppliers.OrderBy(s => s.CompanyName), "SupplierId", "CompanyName", product?.SupplierId);
         }
     }
 }
--- Lesson 16/NorthwindMvc/Models/Product.cs
+++ Lesson 16/NorthwindMvc/Models/Product.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace NorthwindMvc.Models;
 
 public partial class Product
 {
     public int ProductId { get; set; }
-
+    [Required]
+    [MaxLength(50)]
     public string ProductName { get; set; } = null!;
 
     public int? SupplierId { get; set; }
@@ -14,9 +17,10 @@ public partial class Product
     public int? CategoryId { get; set; }
 
     public string? QuantityPerUnit { get; set; }
-
+    [Required]
     public decimal? UnitPrice { get; set; }
-
+    [Required]
+    [System.ComponentModel.DataAnnotations.DataType("integer")]
     public short? UnitsInStock { get; set; }
 
     public short? UnitsOnOrder { get; set; }
