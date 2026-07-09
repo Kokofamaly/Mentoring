a1310c306976cc5031ea6720e078e303dc8f30eb
Kokofamaly <fateev1vv2@gmail.com>
2026-07-10 01:13:16 +0300

    Add RestClient that gets list of products, then gets one product and updates its name

---
+++ Lesson 17/RestClient/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using RestClient;
+using System.Net.Http.Json;
+
+IConfiguration config = new  ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: false, reloadOnChange: true).AddEnvironmentVariables().Build();
+
+string url = config["Url"];
+
+using var client = new HttpClient() { BaseAddress = new Uri(url) };
+
+try
+{
+    var response = await client.GetAsync("api/products");
+
+    if (response.IsSuccessStatusCode)
+    {
+        var prods = await response.Content.ReadFromJsonAsync<IEnumerable<ProductModel>>();
+        foreach(var p in prods)
+        {
+            Console.WriteLine(p.ProductName);
+        }
+
+        var prod = prods.FirstOrDefault();
+
+        if (prod == null) return;
+
+        prod.ProductName = prod.ProductName.Replace("- old", "- updated");
+
+        response = await client.PutAsJsonAsync("api/products/" + prod.ProductId, prod);
+        
+        Console.WriteLine(response.StatusCode);
+
+        var updatedProd = await (await client.GetAsync($"api/products/{prod.ProductId}")).Content.ReadFromJsonAsync<ProductModel>();
+
+        Console.WriteLine(updatedProd?.ProductName);
+
+
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
