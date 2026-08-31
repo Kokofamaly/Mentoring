77138bd53593795aa7183d8abe4cf7c7c738d379
Kokofamaly <fateev1vv2@gmail.com>
2026-08-31 15:29:25 +0200

    start styling

--- ExamProject/React/my-app/src/Sessions.tsx
+++ ExamProject/React/my-app/src/Sessions.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { useOptimistic, useState } from "react";
+import "./Sessions.css";
 
 interface Session{
     id: string,
@@ -16,25 +17,32 @@ interface SessionCardProps{
 export function Sessions(){
     const [sessionList, setSessionList] = useState<Array<Session>>([]);
     const [optimisticSessionList, setOptimisticSessionList] = useOptimistic(sessionList);
-    
+
     // const [isPending, error, data] = useQuery({
     //     queryKey: ['sessionList'],
     //     queryFn: () => fetch("/learningsession").then(res => res.json())
     // });
 
+    function handleAdd(newSession: Session){}
+
     return (
-        <ul>{[...optimisticSessionList]
-            .sort((a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime())
-            .map(s => 
-                <SessionCard session={s}/>
-        )}</ul>
+        <div className="sessions area">
+            <ul>{[...optimisticSessionList]
+                .sort((a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime())
+                .map(s => 
+                    <SessionCard session={s}/>
+            )}</ul>
+        </div>
     );
 }
 
 function SessionCard({ session } : SessionCardProps){
     const sessionCreatedAt = new Date(session.createdAt);
+
+    function handleDelete(sessionId: string){}
+
     return(
-        <li className="sessionCard">
+        <li className="session card">
             <span className="id">{session.id}</span>
             <span className="date">{sessionCreatedAt.getDate()}.{sessionCreatedAt.getMonth() + 1}.{sessionCreatedAt.getFullYear()}</span>
             {session.language && <span className="language">{session.language}</span>}
--- ExamProject/React/my-app/src/Words.tsx
+++ ExamProject/React/my-app/src/Words.tsx
@@ -1,18 +1,45 @@
 import { useDeferredValue, useOptimistic, useState } from "react";
+import "./Words.css";
+
+interface Word{
+    id: string,
+    word: string,
+    translation: string,
+    language: string,
+    category?: string,
+    usageExample?: string
+}
 
 
 export function Words(){
-    const [wordList, setWordList] = useState([]);
+    const [wordList, setWordList] = useState<Array<Word>>([]);
     const [optimisticWordList, setOptimisticWordList] = useOptimistic(wordList);
-    const [searchWord, setSearchWord] = useState<string>();
-    const [selectedWord, setSelectedWord] = useState(null);
-    const filteredWordList = wordList.filter(w => w.word.toLowerCase().startsWith(searchWord.toLowerCase()));
-    return(<div>
-        <button>Add word</button>
-        <input type="text" placeholder="search..." value={searchWord} onChange={e => {setSearchWord(e.target.value); }}/>
-        <ul>
-            {searchWord ? filteredWordList.map(w => <li>w.word</li>) : optimisticWordList.map(w => <li>w.word</li>)}
-        </ul>
+    const [searchWord, setSearchWord] = useState<string>("");
+    const [selectedWordId, setSelectedWordId] = useState<string | null>(null);
 
-    </div>);
+    const selectedWord = optimisticWordList.find(w => w.id === selectedWordId);
+    const filteredWordList: Array<Word> = wordList.filter(w => w.word.toLowerCase().startsWith(searchWord.toLowerCase()));
+    
+    function handleEdit(wordId: string, updatedWord: Word){}
+    function handleDelete(wordId: string){}
+    function handleAdd(newWord: Word){}
+
+    return(
+        <div className="words area">
+            <button>Add word</button>
+            <input type="text" placeholder="search..." value={searchWord} onChange={e => setSearchWord(e.target.value)}/>
+            <ul>
+                {searchWord 
+                ? filteredWordList.map(w => <li onClick={() => setSelectedWordId(w.id)}>{w.word}</li>) 
+                : optimisticWordList.map(w => selectedWord === w 
+                    ? <li className="wordcard selected" onClick={() => setSelectedWordId(null)}>
+                            <span>{w.word}</span>
+                            <span>{w.translation}</span>
+                            <button>Edit</button>
+                            <button>Delete</button>
+                        </li>
+                    : <li className="wordcard" onClick={() => setSelectedWordId(w.id)}>{w.word}</li>)}
+            </ul>
+
+        </div>);
 }
